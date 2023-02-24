8de964394fdf693118f6275818e61ef044c2c89b
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-02-24 16:30:04 +0000

    DPR-184 tidy up partitioner script

--- digital-prisons-reporting/cloud-platform/script/spike/DMSEventRouter.scala
+++ digital-prisons-reporting/cloud-platform/script/spike/DMSEventRouter.scala
@@ -5,7 +5,7 @@ import com.amazonaws.services.glue.GlueContext
 import com.amazonaws.services.glue.util.{GlueArgParser, Job, JsonOptions}
 import com.amazonaws.services.kinesis.AmazonKinesisClientBuilder
 import com.amazonaws.services.kinesis.clientlibrary.lib.worker.InitialPositionInStream
-import com.amazonaws.services.kinesis.model.{PutRecordRequest, PutRecordsRequest, PutRecordsRequestEntry}
+import com.amazonaws.services.kinesis.model.{PutRecordsRequest, PutRecordsRequestEntry}
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import org.apache.spark.SparkContext
@@ -28,6 +28,9 @@ object GlueApp {
   val loadEventStream = "dpr-kinesis-partitioned-load-events-spike"
   val cdcEventStream = "dpr-kinesis-partitioned-cdc-events-spike"
 
+  // This is the upper limited allowed by Kinesis for batched put requests.
+  val maxBatchSize = 500
+
   // Client used for publishing messages
   val kinesisClient = AmazonKinesisClientBuilder.standard()
     .withCredentials(credentialsProvider)
@@ -48,7 +51,6 @@ object GlueApp {
 
     Job.init(args("JOB_NAME"), glueContext, args.asJava)
 
-    // TODO - can we express the schema of the incoming data correctly?
     val kinesisConsumerOptions =
       """{
         |"typeOfData": "kinesis",
@@ -68,11 +70,10 @@ object GlueApp {
 
     glueContext.forEachBatch(dataFrame, (dataFrame: Dataset[Row], batchId: Long) => {
       if (dataFrame.count() > 0) {
-        println(s"Batch: $batchId Processing ${dataFrame.count()} records")
         val startTime = System.currentTimeMillis()
 
         val putRequests = dataFrame.collectAsList().asScala.map { row =>
-          val data = row.getString(0)
+          val data: String = row.getString(0)
 
           val parsed: Map[String, Map[String, String]] = reader.readValue(data)
 
@@ -81,45 +82,44 @@ object GlueApp {
           val partitionKey = metadata.flatMap(_.get("partitionKey")).getOrElse(UUID.randomUUID().toString)
 
           metadata.flatMap(_.get("operation")) match {
-            case Some("load") => (loadEventStream, Some(putRequestEntry(partitionKey, data)))
+            case Some("load") => (loadEventStream, Some(createPutRequestEntry(partitionKey, data)))
+            // Filter out create-table events for now.
             case Some("create-table") => (loadEventStream, None)
-            case _ => (cdcEventStream, Some(putRequestEntry(partitionKey, data)))
+            // Treat all other event types as CDC events e.g. insert, update, delete
+            case _ => (cdcEventStream, Some(createPutRequestEntry(partitionKey, data)))
           }
         }
 
-        // TODO - consider factoring this out
-        val loadRequests = putRequests.filter(_._1 == loadEventStream).flatMap(_._2)
-        val cdcRequests = putRequests.filter(_._1 == cdcEventStream).flatMap(_._2)
-
-        val maxBatchSize = 500
-
-        loadRequests.grouped(maxBatchSize).foreach { g =>
-          val loadRecordsRequest = new PutRecordsRequest
-          loadRecordsRequest.setRecords(g.asJava)
-          loadRecordsRequest.setStreamName(loadEventStream)
-          kinesisClient.putRecords(loadRecordsRequest)
-        }
-
-        cdcRequests.grouped(maxBatchSize).foreach { g =>
-          val cdcRecordsRequest = new PutRecordsRequest
-          cdcRecordsRequest.setRecords(g.asJava)
-          cdcRecordsRequest.setStreamName(cdcEventStream)
-          kinesisClient.putRecords(cdcRecordsRequest)
-        }
+        putRequestsForStream(loadEventStream, putRequests)
+        putRequestsForStream(cdcEventStream, putRequests)
 
         val runTime = System.currentTimeMillis() - startTime
-        println(s"Batch: $batchId Processed ${dataFrame.count} records in ${runTime}ms - ${BigDecimal(runTime.toDouble / dataFrame.count).setScale(3, RoundingMode.HALF_UP)}ms per record")
+        val timePerRecord = BigDecimal(runTime.toDouble / dataFrame.count).setScale(3, RoundingMode.HALF_UP)
+
+        println(s"Batch: $batchId Processed ${dataFrame.count} records in ${runTime}ms - ${timePerRecord}ms per record")
       }
     }, JsonOptions(s"""{"windowSize" : "5 seconds", "checkpointLocation" : "${args("TempDir")}/${args("JOB_NAME")}/checkpoint/"}"""))
     Job.commit()
   }
 
-  // TODO - handle publish failures and rework to batch to minimise number of kinesis client calls
-  def putRequestEntry(partitionKey: String, data: String) = {
+  private def createPutRequestEntry(partitionKey: String, data: String) = {
     val entry = new PutRecordsRequestEntry
     entry.setData(ByteBuffer.wrap(data.getBytes))
     entry.setPartitionKey(partitionKey)
     entry
   }
+
+  // TODO - handle client errors
+  private def putRequestsForStream(streamName: String, requests: Seq[(String, Option[PutRecordsRequestEntry])]): Unit =
+    requests
+      .filter(_._1 == streamName)
+      .flatMap(_._2)
+      .grouped(maxBatchSize)
+      .foreach { batch =>
+        val putRecordsRequest = new PutRecordsRequest
+        putRecordsRequest.setRecords(batch.asJava)
+        putRecordsRequest.setStreamName(streamName)
+        kinesisClient.putRecords(putRecordsRequest)
+      }
 
 }
